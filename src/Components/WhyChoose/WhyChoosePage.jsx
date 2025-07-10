4152de75b1be0ca492cb59e93e0d9c80a4ef3fa7
Kartik Garg <gargkartik503@gmail.com>
2025-07-10 18:24:33 +0530

    mobile view fixed

--- src/Components/WhyChoose/WhyChoosePage.jsx
+++ src/Components/WhyChoose/WhyChoosePage.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import DotGrid from './DotGridBackground'
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
