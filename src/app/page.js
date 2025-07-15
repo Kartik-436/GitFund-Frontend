382c9abdd1f0e5ee5da9e774e735d8cf8834327c
Kartik Garg <gargkartik503@gmail.com>
2025-07-15 10:16:47 +0530

    deployment fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
 'use client'
 import PricingPage from "../components/Pricing Page/PricingPage"
 import SmoothScroll from "./SmoothScroll"
-<<<<<<< HEAD
 import TestimonialsPage from '../components/Testimonials/TestimonialsPage';
 import WhyChoosePage from '../components/WhyChoose/WhyChoosePage';
 import LastPortal from '../components/End/LastPortal';
@@ -11,20 +10,7 @@ import BentoGridDemo from '../components/Bounties/BountiesSection';
 import Footer from '../components/Footer/page';
 import { ThemeProvider } from '../components/End/ThemeChangeContext';
 import NavBar3 from '../components/NavBar/NavBar3';
-import { LayoutWithPreloader } from "../components/LayoutWithPreloader";
 import MaskedPage from "@/Components/Home/PortalReveal";
-=======
-import TestimonialsPage from './../Components/Testimonials/TestimonialsPage';
-import WhyChoosePage from './../Components/WhyChoose/WhyChoosePage';
-import MaskedPage from "./../Components/Home/PortalReveal"
-import LastPortal from './../Components/End/LastPortal';
-import FinalPage from './../Components/End/FinalPage';
-import NavBarAll from './../Components/NavBar/NavBarAll';
-import BentoGridDemo from './../Components/Bounties/BountiesSection';
-import Footer from './../Components/Footer/Footer';
-import { ThemeProvider } from './../Components/End/ThemeChangeContext';
-import NavBar3 from './../Components/NavBar/NavBar3';
->>>>>>> parent of 562c24b (Add animated preloader with particle text effect)
 
 export default function Home() {
   return (
