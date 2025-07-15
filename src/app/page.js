2f70e6b5956ab4386cf2342bc4f83b3c12feef58
Kartik Garg <gargkartik503@gmail.com>
2025-07-15 10:19:44 +0530

    deployment fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,15 @@
 'use client'
-import PricingPage from "../components/Pricing Page/PricingPage"
+import PricingPage from "../Components/Pricing Page/PricingPage"
 import SmoothScroll from "./SmoothScroll"
-import TestimonialsPage from '../components/Testimonials/TestimonialsPage';
-import WhyChoosePage from '../components/WhyChoose/WhyChoosePage';
-import LastPortal from '../components/End/LastPortal';
-import FinalPage from '../components/End/FinalPage';
-import NavBarAll from '../components/NavBar/NavBarAll';
-import BentoGridDemo from '../components/Bounties/BountiesSection';
-import Footer from '../components/Footer/page';
-import { ThemeProvider } from '../components/End/ThemeChangeContext';
-import NavBar3 from '../components/NavBar/NavBar3';
+import TestimonialsPage from '../Components/Testimonials/TestimonialsPage';
+import WhyChoosePage from '../Components/WhyChoose/WhyChoosePage';
+import LastPortal from '../Components/End/LastPortal';
+import FinalPage from '../Components/End/FinalPage';
+import NavBarAll from '../Components/NavBar/NavBarAll';
+import BentoGridDemo from '../Components/Bounties/BountiesSection';
+import Footer from '../Components/Footer/page';
+import { ThemeProvider } from '../Components/End/ThemeChangeContext';
+import NavBar3 from '../Components/NavBar/NavBar3';
 import MaskedPage from "@/Components/Home/PortalReveal";
 
 export default function Home() {
